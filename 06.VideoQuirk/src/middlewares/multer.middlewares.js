d562c42e4b73fa5834079e2956830f2c4aaf368d
Nikhil <ace.nikchauhan@gmail.com>
2024-12-31 12:38:20 +0530

    videoQuirk project: added multer for file upload and cloudinary to store vid/img on cloud

---
+++ 06.VideoQuirk/src/middlewares/multer.middlewares.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname); // Get file extension (e.g., .jpg, .png)
+    const newFileName = `user-upload-${uniqueSuffix}${ext}`;
+    cb(null, newFileName);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
