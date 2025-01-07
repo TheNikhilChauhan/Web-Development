8b94d65ba593696a15c3afba56850c90611fd146
Nikhil <ace.nikchauhan@gmail.com>
2025-01-07 16:46:55 +0530

    useMemo: added new assignment

---
+++ 09.Project-Assignments/02.use-memo/src/App.jsx
@@ -0,0 +1,16 @@
+import { Assignment1 } from "./components/Assignment1";
+import "./App.css";
+import { Assignment3 } from "./components/Assignment3";
+import { Assignment2 } from "./components/Assignment2";
+
+function App() {
+  return (
+    <>
+      {/* <Assignment1 /> */}
+      <Assignment2 />
+      {/* <Assignment3 /> */}
+    </>
+  );
+}
+
+export default App;
