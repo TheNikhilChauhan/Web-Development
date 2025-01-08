dd4b502034f9ec2604f25347a460a70c5c4b3833
Nikhil <ace.nikchauhan@gmail.com>
2025-01-08 11:28:48 +0530

    state Management: added recoil part 1

---
+++ 07.React/07.StateManagement/src/App.jsx
@@ -0,0 +1,51 @@
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import "./App.css";
+import { countAtom } from "./store/atoms/count";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
